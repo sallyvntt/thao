d02a532556f3a9a5cfd90a4182e3850271afb25a
sallyvntt <106750052+sallyvntt@users.noreply.github.com>
2023-09-27 23:02:41 +0700

    mai làm phân quyền sao

--- Group3/Controllers/ProductController.cs
+++ Group3/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using Group3.Reponsitory;
 using Group3.Services;
 using Lib;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Group3.Controllers
 {
@@ -26,15 +27,27 @@ namespace Group3.Controllers
         [HttpGet]
       public  IActionResult Create()
         {
-            IEnumerable<CatMst> catMst = databaseContext.CatMsts.ToList();
-            ViewBag.CatMst = catMst;
-          
+            List<CatMst> cate= databaseContext.CatMsts.ToList();
+            // Chuyển đổi danh sách CatMst thành danh sách SelectListItem
+            List<SelectListItem> categoryItems = cate.Select(c => new SelectListItem
+            {
+                Value = c.Cat_ID,
+                Text = c.Cat_Name
+            }).ToList();
+            ViewBag.Categories = categoryItems;
+            List<BrandMst>  brands = databaseContext.BrandMsts.ToList();
+            List<SelectListItem> brandItem = brands.Select(c => new SelectListItem
+            {
+                Value= c.Brand_ID,
+                Text = c.Brand_Type
+            }).ToList();
+            ViewBag.Brand_ID = brandItem;
             return View();
         }
         [HttpPost]
         public async Task<IActionResult> Create(Product product  )
         {
-            
+            productRepository.CreateAsync(product);
             return View();
         }
 
